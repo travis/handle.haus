18e68b6a17de95ded0418a65d0a6f4dc2bc50018
Travis Vachon <travis.vachon@gmail.com>
2020-03-25 13:30:22 -0700

    redirects act weird with browsers, change to serving a ttl

--- pages/api/handles/[handle].js
+++ pages/api/handles/[handle].js
@@ -15,8 +15,7 @@ function runMiddleware(req, res, fn) {
 }
 
 const corsMiddleware = cors({
-  methods: "GET,HEAD",
-  exposedHeaders: "Location"
+  methods: "GET,HEAD"
 })
 
 const handles = {
@@ -25,12 +24,20 @@ const handles = {
 
 export default async function handleRequest(req, res) {
   await runMiddleware(req, res, corsMiddleware)
+  const handle = req.query.handle
+  const webId = handles[handle]
+  if (handles[handle]) {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/turtle')
+    res.setHeader('Cache-Control', 'max-age=60, s-maxage=86400, public')
+    res.end(`@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/>.
 
-  if (handles[req.query.handle]) {
-    res.statusCode = 302
-    res.setHeader('Location', handles[req.query.handle])
+<https://handle.haus/handles/${handle}> owl:sameAs <${webId}> .
+<https://handle.haus/handles/${handle}> foaf:nick "${handle}".
+`)
   } else {
     res.statusCode = 404
+    res.end()
   }
-  res.end()
 }
